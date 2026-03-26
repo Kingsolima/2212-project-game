31e45f61cf247fbd5f3e58015c9b430494d5fb25
Fardin Abbassi <fabbassi@uwo.ca>
2026-03-26 22:42:53 +0000

    Update 4 files
    - /src/main/java/ca/uwo/cs2212/group54/stayingalive/ui/MainMenuScreen.java
    - /src/main/java/ca/uwo/cs2212/group54/stayingalive/ui/NavigationControl.java
    - /src/main/java/ca/uwo/cs2212/group54/stayingalive/ui/Screen.java
    - /src/main/java/ca/uwo/cs2212/group54/stayingalive/ui/PlayerScreen.java

--- src/main/java/ca/uwo/cs2212/group54/stayingalive/ui/MainMenuScreen.java
+++ src/main/java/ca/uwo/cs2212/group54/stayingalive/ui/MainMenuScreen.java
@@ -1,3 +1,14 @@
+/**
+ * MainMenuScreen class represents the main menu screen of Staying Alive.
+ * <p>
+ * MainMenuScreen implements the Screen interface, 
+ * so it has methods to show the screen, move to the next screen, 
+ * and get the class' frame.
+ * Since Screen implements ActionListener, it also has a method to handle button clicks.
+ * 
+ * @author Fardin Abbassi
+ */
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;;
@@ -14,7 +25,6 @@ public class MainMenuScreen implements Screen {
     static Color buttonTextColor = Color.black;
 
     // Navigation Buttons
-    private JButton exitButton = new JButton("Exit");
     private JButton loginButton = new JButton("Login");
     private JButton tutorialButton = new JButton("Tutorial");
     private JButton parentalControlButton = new JButton("Parental Controls");
@@ -25,73 +35,70 @@ public class MainMenuScreen implements Screen {
     private JLabel credits = new JLabel();
     
 
-    /** 
+    /** ADD DESCRIPTION HERE
+     * Handle button clicks on main menu
+     * <p>
+     * Exit button exits application, login button moves to login screen, 
+     * tutorial button moves to tutorial screen, and 
+     * parental control button moves to parental control screen.
+     * <p>
+     * 
      * @param e
      */
     // 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-        // Exit if exit button is pressed
-        if (e.getSource() == exitButton) {
-            System.exit(0);
-        }
-
         // Switch to login screen if login button is pressed
-        else if (e.getSource() == loginButton) {
-            this.moveToNextScreen(null);
-            
-        }
-
+        if (e.getSource() == loginButton) {this.moveToNextScreen("login");}
         // Switch to tutorial screen if tutorial button is pressed
-        else if (e.getSource() == tutorialButton) {
-
-        }
-
+        else if (e.getSource() == tutorialButton) {this.moveToNextScreen("tutorial");}
         // Switch to parental control screen if parental control button is pressed
-        else if (e.getSource() == parentalControlButton) {
-
-        }
+        else if (e.getSource() == parentalControlButton) {this.moveToNextScreen("parental control");}
     }
 
-    
+    /** ADD DESCRIPTION HERE
+     * 
+     */
     @Override
     public void showScreen() {
         // Title
 		title.setText("Staying Alive");
-		title.setFont(new Font("Helvetica", Font.PLAIN, 30)); // adjust as needed
+		title.setFont(new Font("Helvetica", Font.PLAIN, 60)); // adjust as needed
 		title.setForeground(titleTextColour);
-		title.setBounds(50, 20, 300, 40); // adjust as needed
+		title.setBounds(225, 20, 400, 80); // adjust as needed
         subLabel.setText("A Shebab Kebab Creation");
-        subLabel.setFont(new Font("Helvetica", Font.PLAIN, 16)); // adjust as needed
+        subLabel.setFont(new Font("Helvetica", Font.PLAIN, 20)); // adjust as needed
         subLabel.setForeground(titleTextColour);
-        subLabel.setBounds(100, 60, 300, 20); // adjust as needed
+        subLabel.setBounds(285, 100, 300, 20); // adjust as needed
+
+        // TODO: Add Logo in the middle of the frame(?)
 
 		// Set Up Login Button
-		loginButton.setFont(new Font("Helvetica", Font.PLAIN, 16)); // adjust as needed
-		loginButton.setForeground(buttonTextColor);
+		loginButton.setFont(new Font("Helvetica", Font.PLAIN, 20)); // adjust as needed
+        loginButton.setForeground(buttonTextColor);
 		loginButton.setBackground(buttonBackground);
-		loginButton.setBounds(125, 200, 100, 25); // adjust as needed
+		loginButton.setBounds(300, 250, 200, 30); // adjust as needed
 		loginButton.addActionListener(this);
 	
 		// Set Up Tutorial Button
-		tutorialButton.setFont(new Font("Helvetica", Font.PLAIN, 16)); // adjust as needed
+		tutorialButton.setFont(new Font("Helvetica", Font.PLAIN, 20)); // adjust as needed
 		tutorialButton.setForeground(buttonTextColor);
 		tutorialButton.setBackground(buttonBackground);
-		tutorialButton.setBounds(125, 200, 100, 25); // adjust as needed
+		tutorialButton.setBounds(50, 250, 200, 30); // adjust as needed
 		tutorialButton.addActionListener(this);
 
         // Set Up Parental Control Button
-        tutorialButton.setFont(new Font("Helvetica", Font.PLAIN, 16)); // adjust as needed
-		tutorialButton.setForeground(buttonTextColor);
-		tutorialButton.setBackground(buttonBackground);
-		tutorialButton.setBounds(125, 200, 100, 25); // adjust as needed
-		tutorialButton.addActionListener(this);
+        parentalControlButton.setFont(new Font("Helvetica", Font.PLAIN, 20)); // adjust as needed
+		parentalControlButton.setForeground(buttonTextColor);
+		parentalControlButton.setBackground(buttonBackground);
+		parentalControlButton.setBounds(550, 250, 200, 30); // adjust as needed
+		parentalControlButton.addActionListener(this);
 
         // Set Up Credits
-        credits.setText("Created by Omar Soliman, Osman Idris, Mohamed Ahmed, Malik Alghneimin, and Fardin Abbassi as Group 54 for the CS 2212 course in the Winter 2026 term.");
-        credits.setFont(new Font("Helvetica", Font.PLAIN, 10)); // adjust as needed
+        credits.setText("<html>Created by Omar Soliman, Osman Idris, Mohamed Ahmed, Malik Alghneimin, and Fardin Abbassi as Group 54 for the CS 2212 course in the Winter 2026 term.</html>");
+        credits.setFont(new Font("Helvetica", Font.PLAIN, 15)); // adjust as needed
         credits.setForeground(Color.white);
-        credits.setBounds(10, 350, 400, 20); // adjust as needed
+        credits.setBounds(150, 325, 550, 50); // adjust as needed
 		
 		// add to frame
 		mainMenuFrame.getContentPane().add(title);
@@ -105,19 +112,30 @@ public class MainMenuScreen implements Screen {
 		mainMenuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainMenuFrame.setSize(420, 420);
 		mainMenuFrame.getContentPane().setLayout(null);
-		mainMenuFrame.getContentPane().setBackground(new Color(220, 242, 206));
-		mainMenuFrame.setIconImage(Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/shebab_icon.png"))); // Shebab Kebab logo without text, adjust as needed
+		mainMenuFrame.getContentPane().setBackground(backgroundPurple);
+		//mainMenuFrame.setIconImage(Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/shebab_icon.png"))); // Shebab Kebab logo without text, adjust as needed
 		mainMenuFrame.setBackground(backgroundPurple); // adjust as needed
+        mainMenuFrame.setForeground(backgroundPurple);
 		mainMenuFrame.setVisible(true);
     }
 
-    /** 
+    /** ADD DESCRIPTION HERE
      * @param screenToMoveTo
      * @return Screen
      */
     @Override
-    public Screen moveToNextScreen(String screenToMoveTo) {
-        // return next screen here
-        return null;
+    public void moveToNextScreen(String screenToMoveTo) {
+        // Open login screen as a pop-up
+        if (screenToMoveTo.equals("login")) {NavigationControl.setCurrentScreen(1);}
+        // Open tutorial screen
+        else if (screenToMoveTo.equals("tutorial")) {NavigationControl.setCurrentScreen(2);}
+        // Open parental control screen
+        else if (screenToMoveTo.equals("parental control")) {NavigationControl.setCurrentScreen(3);}
     }
+
+    /** ADD DESCRIPTION HERE
+     * 
+     */
+    @Override
+    public JFrame getFrame() {return mainMenuFrame;}
 }
--- src/main/java/ca/uwo/cs2212/group54/stayingalive/ui/NavigationControl.java
+++ src/main/java/ca/uwo/cs2212/group54/stayingalive/ui/NavigationControl.java
@@ -1,3 +1,61 @@
+/**
+ * NavigationControl class both controls the screen navigation and the origin point of the application.
+ * <p>
+ * NavigationControl initializes all screens, then starts at the main menu screen. 
+ * It also has a method to update the current screen, 
+ * and a method to set the current screen to another and update.
+ * 
+ * @author Fardin Abbassi
+ */
+
 public class NavigationControl {
-    // add stuff here
+    // Screen Control
+    private static Screen[] listOfScreens = new Screen[8];
+    private static Screen currentScreen;
+
+    // Screen Dimensions
+    public static int screenW = 800; //sets the screen width
+    public static int screenH = 450; //sets the screen height
+
+    /**
+     * Updates the current screen by showing it and setting its size to defined dimensions
+     * @author Fardin Abbassi
+     */
+    private static void updateScreen() {
+        currentScreen.showScreen();
+        currentScreen.getFrame().setSize(screenW, screenH);
+    }
+    /**
+     * Sets current screen to the screen at the index of the list of screens and updates screen to match.
+     * Disposes of the previous screen to prevent memory leak.
+     * @author Fardin Abbassi
+     * @param int screenToSet
+     */
+    public static void setCurrentScreen(int screenToSet) {
+        if (currentScreen != null || screenToSet == 1) currentScreen.getFrame().dispose();
+        currentScreen = listOfScreens[screenToSet];
+        updateScreen();
+    }
+    
+    /**
+     * Constructor for NavigationControl. Initializes screens, then starts at the main menu screen.
+     * @author Fardin Abbassi
+     */
+    public NavigationControl() {
+        // Initialize screens (add as screens are implemented)
+        MainMenuScreen mainMenu = new MainMenuScreen();
+        LoginScreen loginScreen = new LoginScreen();
+
+        // Add screens to list of screens (add as screens are implemented)
+        listOfScreens[0] = mainMenu;
+        listOfScreens[1] = loginScreen;
+
+        // Start at main menu
+        setCurrentScreen(0);
+    }
+
+    // Use as driver for application
+    public static void main(String[] args) {
+        new NavigationControl();
+    }
 }
