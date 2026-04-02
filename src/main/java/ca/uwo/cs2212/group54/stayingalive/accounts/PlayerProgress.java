74314c9265657e946d0413a998f301b9fb27dc92
Osman Mohammed Idris <oidris@uwo.ca>
2026-04-02 15:32:38 -0400

    minor updates on playerprogress

--- src/main/java/ca/uwo/cs2212/group54/stayingalive/accounts/PlayerProgress.java
+++ src/main/java/ca/uwo/cs2212/group54/stayingalive/accounts/PlayerProgress.java
@@ -1,35 +1,48 @@
 package ca.uwo.cs2212.group54.stayingalive.accounts;
 
-import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class PlayerProgress {
+    @JsonIgnore
     public static final int MAX_LEVELS = 8;
-    int currentLevel;
-    boolean[] completedLevels;
-    LocalDate lastSaveDate;
+    @JsonProperty("current_level")
+    private int currentLevel;
+    @JsonProperty("completed_levels")
+    private boolean[] completedLevels;
+
+    /*@JsonCreator
+    public PlayerProgress(@JsonProperty int currentLevel, 
+    @JsonProperty boolean[] completedLevels, @JsonProperty LocalDate lastSaveDate) {
+        this.currentLevel = currentLevel;
+        this.completedLevels = completedLevels;
+        this.lastSaveDate = lastSaveDate;
+    }*/
 
     public PlayerProgress() {
         currentLevel = 1;
         completedLevels = new boolean[MAX_LEVELS];
-        lastSaveDate = LocalDate.now();
     }
 
+    
     public void completeLevel(int level) {
-        completedLevels[level] = true;
+        completedLevels[level-1] = true;
+        //Parental.saveAccountData();
     }
 
+    @JsonProperty("current_level")
+    public int getCurrentLevel() {
+        return currentLevel;
+    }
+
+    @JsonProperty("current_level")
     public void setCurrentLevel(int level) {
         currentLevel = level;
-    }
-
-    public void updateSaveTime() {
-        lastSaveDate = LocalDate.now();
     }
 
     public void clearProgress() {
         int currentLevel = 1;
         completedLevels = new boolean[MAX_LEVELS];
-        lastSaveDate = LocalDate.now();
     }
 
 }
