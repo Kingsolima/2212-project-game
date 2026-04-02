b0edcccee8361bf851298795e2fdf8cba8ead15d
Osman Mohammed Idris <oidris@uwo.ca>
2026-04-02 15:31:57 -0400

    json saving ability for levelstats

--- src/main/java/ca/uwo/cs2212/group54/stayingalive/accounts/LevelStatistic.java
+++ src/main/java/ca/uwo/cs2212/group54/stayingalive/accounts/LevelStatistic.java
@@ -1,16 +1,40 @@
 package ca.uwo.cs2212.group54.stayingalive.accounts;
 
-public class LevelStatistic {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class LevelStatistic {
+    @JsonIgnore
     private LevelData levelData;
-    int wordsPerMinute;
-    int mistakes;
-    int highscore;
-    int attempts;
-    Level_status status;
+    @JsonProperty("wpm")
+    protected int wordsPerMinute;
+    @JsonProperty("mistakes")
+    protected int mistakes;
+    @JsonProperty("highscore")
+    protected int highscore;
+    @JsonProperty("attempts")
+    protected int attempts;
+    @JsonProperty("status")
+    protected Level_status status;
+
+    /*@JsonCreator
+    public LevelStatistic(@JsonProperty int wordsPerMinute,
+    @JsonProperty int mistakes, @JsonProperty int highscore,
+    @JsonProperty int attempts, @JsonProperty String status) {
+        this.wordsPerMinute = wordsPerMinute;
+        this.mistakes = mistakes;
+        this.highscore = highscore;
+        this.attempts = attempts;
+        this.status = Level_status.valueOf(status);
+    }*/
 
     public LevelStatistic(LevelData levelData) {
         this.levelData = levelData;
+        mistakes = wordsPerMinute = highscore = attempts = 0;
+        status = Level_status.UNLOCKED;
+    }
+
+    public LevelStatistic() {
         mistakes = wordsPerMinute = highscore = attempts = 0;
         status = Level_status.UNLOCKED;
     }
@@ -24,8 +48,8 @@ public class LevelStatistic {
     }
 
     public void clearStatistics() {
-            mistakes = wordsPerMinute = highscore = attempts = 0;
-            status = Level_status.LOCKED;
+        mistakes = wordsPerMinute = highscore = attempts = 0;
+        status = Level_status.LOCKED;
     }
 
 }
